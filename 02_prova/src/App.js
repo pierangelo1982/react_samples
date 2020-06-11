ae43f438fb704c802aba780cda757b08ef3478da
pierangelo1982 <pierangelo1982@gmail.com>
2020-06-11 19:06:04 +0200

    02

---
+++ 02_prova/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h2', null, 'ciao'))
+
+  }
+
+}
+
+export default App;
