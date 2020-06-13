58b959d51904658fd6b395c2c6b32c46863981a2
pierangelo1982 <pierangelo1982@gmail.com>
2020-06-13 18:05:10 +0200

    05_components_props_state

--- 04_components_props/src/App.js
+++ 04_components_props/src/App.js
@@ -6,13 +6,21 @@ import Person from './Components/Person'
 class App extends Component {
   render() {
 
+    state=  {
+      person: [
+        {name: 'Pierangelo', age: '38'},
+        {name: 'Marco', age: '35'},
+        {name: 'Donatella', age: '64'},
+      ]
+    }
+
     return (
     
-    <div className="App">
-        <h1>Hi! I'm a React App!</h1>
-        <Person name="Perangelo" age="38" />
-        <Person name="Marco" age="35" />
-        <Person name="Donatella" age="54">Ciao Mamma</Person>
+      <div className="App">
+          <h1>Hi! I'm a React App!</h1>
+          <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+          <Person name={this.state.person[1].name} age={this.state.person[1].age} />
+          <Person name={this.state.person[2].name} age={this.state.person[2].age}>Ciao Mamma</Person>
       </div>
 
     );
